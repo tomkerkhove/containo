e71f2b1bcfd2ce0544edefcefbf86ffc498396b1
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-05-30 16:50:09 +0200

    Add empty projects
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Containo.Services.Orders.QueueProcessor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Containo.Services.Orders.QueueProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
