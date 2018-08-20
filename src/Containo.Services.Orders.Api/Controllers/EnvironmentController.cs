285fdb91d80d478142e59bf3a8282f3c40fde99c
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-08-20 08:45:26 +0200

    Provide controller to get full value of environment value
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Containo.Services.Orders.Api/Controllers/EnvironmentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Containo.Services.Orders.Api.Controllers
+{
+    [Route("api")]
+    public class EnvironmentController : Controller
+    {
+        /// <summary>
+        ///     Provides information about the environment
+        /// </summary>
+        /// <param name="environmentVariableName">Name of the environment variable that you are interested in</param>
+        [Route("environment/{environmentVariableName}")]
+        [HttpGet]
+        [SwaggerResponse((int)HttpStatusCode.OK, description: "Information about the environment",
+            type: typeof(string))]
+        [SwaggerResponse((int)HttpStatusCode.NotFound, description: "Order was not found")]
+        public IActionResult Get(string environmentVariableName)
+        {
+            if (string.IsNullOrWhiteSpace(environmentVariableName))
+            {
+                return BadRequest("No environment variable was specified");
+            }
+
+            var environmentVariable = Environment.GetEnvironmentVariable(environmentVariableName);
+            if (string.IsNullOrWhiteSpace(environmentVariable))
+            {
+                return NotFound();
+            }
+
+            return Ok(environmentVariable);
+        }
+    }
+}
